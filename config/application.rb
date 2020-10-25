1462efb43f0bdf7ecabce95efc077471f5a9767b
bank <jiraphun.jrb@gmail.com>
2020-10-25 17:25:02 +0700

    migrate db

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require_relative 'boot'
+require_relative "boot"
 
 require "rails"
 # Pick the frameworks you want:
@@ -33,5 +33,8 @@ module WorkshopLogin
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+    end
   end
 end
